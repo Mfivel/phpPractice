0faddaf5aa334ad3ac6dfbf71cb6dee1a3444664
Mfivel <Mfivel@hotmail.com>
2021-12-22 14:53:52 -0600

    Add files via upload

---
+++ site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+    //Practicing with php
+     echo("Hello World");
+     //Header
+    //echo "<h1>Michelle web site practice</h1>";
+
+    ?>
+
+  </body>
+</html>
